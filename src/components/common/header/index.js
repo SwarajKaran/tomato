f7fa1f0f97f29cf6ffcdc183a92047edc5265130
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-28 02:32:31 +0530

    adding header logo

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 import './header.css';
 
 const Header = () => {
-  return <div>This is Header</div>;
+  return (
+    <div className="max-width header">
+      <img
+        src="https://o.remove.bg/downloads/e9e7c785-2688-466b-8964-ddada3f991a8/tomato_header-removebg-preview.png"
+        alt="tomato-logo"
+        className="header-logo"
+      />
+      <div className="header-left">
+        <div className="header-loaction-search-container">
+          <div className="location-wrapper">
+            <div className="location-icon-name">
+              <i className="fi fi-sr-marker"></i>
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
