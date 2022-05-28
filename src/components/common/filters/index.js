99a3db40d5ed032f6b51232b4dc7d1ceed09fe36
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-28 18:42:21 +0530

    filters components

---
+++ src/components/common/filters/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FilterItem from './filterItem';
+import './filters.css';
+const Filters = ({ filterList }) => {
+  return (
+    <div className="filters">
+      {filterList &&
+        filterList.map((filter) => {
+          return <FilterItem key={filter.id} filter={filter} />;
+        })}
+    </div>
+  );
+};
+
+export default Filters;
--- src/components/delivery/index.js
+++ src/components/delivery/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import Filters from '../common/filters';
 import './delivery.css';
+const deliveryFilters = [
+  {
+    id: 1,
+    icon: <i className="fi fi-rr-settings-sliders"></i>,
+    title: 'Filters',
+  },
+  {
+    id: 2,
+    title: 'Rating: 4.0+',
+  },
+  {
+    id: 3,
+    title: 'Safe and Hygienic',
+  },
+  {
+    id: 4,
+    title: 'Pure Veg',
+  },
+  {
+    id: 5,
+    icon: <i class="fi fi-rr-apps-sort"></i>,
+    title: 'Delivery Time',
+  },
+  {
+    id: 6,
+    title: 'Great Offers',
+  },
+];
 const Delivery = () => {
-  return <div>THis is Delivery</div>;
+  return (
+    <div>
+      <div className="max-width">
+        <Filters filterList={deliveryFilters} />
+      </div>
+    </div>
+  );
 };
 
 export default Delivery;
