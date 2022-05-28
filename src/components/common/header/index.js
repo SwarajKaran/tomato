60716ee41e3671c3036c913035f28b835516f2ee
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-28 12:39:20 +0530

    add location, search, profile and add styles

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -5,19 +5,38 @@ const Header = () => {
   return (
     <div className="max-width header">
       <img
-        src="https://o.remove.bg/downloads/e9e7c785-2688-466b-8964-ddada3f991a8/tomato_header-removebg-preview.png"
+        src="https://i.imgur.com/Yl9qTGn.png"
         alt="tomato-logo"
         className="header-logo"
       />
-      <div className="header-left">
-        <div className="header-loaction-search-container">
+      <div className="header-right">
+        <div className="header-location-search-container">
           <div className="location-wrapper">
             <div className="location-icon-name">
-              <i className="fi fi-sr-marker"></i>
+              <i className="fi fi-rr-marker absolute-center location-icon"></i>
+              <div>Bangalore</div>
             </div>
+
+            <i className="fi fi-rr-caret-down absolute-center"></i>
+          </div>
+          <div className="location-search-seperator"></div>
+          <div className="header-searchbar">
+            <i className="fi fi-rr-search absolute-center search-icon"></i>
+            <input
+              placeholder="Search for restaurant, cuisine or a dish"
+              className="search-input"
+            />
           </div>
         </div>
-        <div></div>
+        <div className="profile-wrapper">
+          <img
+            src="https://b.zmtcdn.com/images/user_avatars/wine_2x.png?fit=around%7C100%3A100&crop=100%3A100%3B%2A%2C%2A"
+            alt="profile"
+            className="header-profile-image"
+          />
+          <span className="header-username">Swaraj</span>
+          <i className="fi fi-rr-angle-down absolute-center profile-options-icon"></i>
+        </div>
       </div>
     </div>
   );
