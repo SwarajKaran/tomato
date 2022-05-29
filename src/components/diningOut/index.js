3320bcabaa8a07c48e28072e32296382a80996cc
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-29 18:35:09 +0530

    create Collection - skeleton only

--- src/components/diningOut/index.js
+++ src/components/diningOut/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Collection from '../common/collection';
 import './diningOut.css';
 
 const DiningOut = () => {
-  return <div>This is DiningOut</div>;
+  return (
+    <div>
+      <Collection />
+    </div>
+  );
 };
 
 export default DiningOut;
